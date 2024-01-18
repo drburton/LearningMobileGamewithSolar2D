682b397171d3595507464b0573c1c996b4e1fad1
Dr. Burton <drburton@burtonsmediagroup.com>
2024-01-18 15:22:42 -0600

    Update config.lua

--- Chapter 6/Ch62 Composer/config.lua
+++ Chapter 6/Ch62 Composer/config.lua
@@ -1,7 +1,4 @@
 -- config.lua for project: Ch62Storyboard
--- Managed with http://OutlawGameTools.com
--- Copyright 2013 Brian Burton. All Rights Reserved.
--- cpmgen config.lua
 application =
 {
 	content =
